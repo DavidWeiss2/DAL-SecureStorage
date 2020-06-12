821041c00dbaaee159858b9e5e9146eae9ecddc0
arnav224 <avraham224@gmail.com>
2020-06-12 18:29:41 +0300

    only one file on the DAL side

--- applet/Notes/src/Notes/SecureStorage.java
+++ applet/Notes/src/Notes/SecureStorage.java
@@ -1,10 +1,13 @@
 package Notes;
 
 import com.intel.util.*;
+import com.intel.crypto.CryptoException;
+import com.intel.crypto.HashAlg;
+import com.intel.crypto.Random;
+import com.intel.crypto.SymmetricBlockCipherAlg;
 import com.intel.langutil.*;
 import java.util.Hashtable;
 import java.util.*; 
-import Notes.Crypto.*;
 
 public class SecureStorage {
 	Hashtable<Integer, Boolean> allFilesNames;  // the original FS (list of file names).
@@ -21,11 +24,11 @@ public class SecureStorage {
 		{
 			int FS_size = TypeConverter.bytesToInt(request, 1);  // number of existingFiles
 			allFilesNames = new Hashtable<Integer, Boolean>(FS_size);
-			int offset;
-			for (offset = 9; offset < 9 + 4 * FS_size; offset += 4)
+			int importedFilesNum = TypeConverter.bytesToInt(request, 5);
+			int offset = 9;
+			for (; offset < 9 + 4 * FS_size; offset += 4)
 				allFilesNames.put(TypeConverter.bytesToInt(request, offset), true);
 
-			int importedFilesNum = TypeConverter.bytesToInt(request, 5);
 			files = new Hashtable<Integer, byte[]>(importedFilesNum);
 			for (int i = 0; i < importedFilesNum; i++) {
 				int fileName = TypeConverter.bytesToInt(request, offset);
@@ -93,20 +96,12 @@ public class SecureStorage {
 		// the user buffer
 		ArrayUtils.copyByteArray(response, 0, result, offset, response.length);
 		//DebugPrint.printString("exiting insertFSInfoToBuffer()." );
-
 		return	result;
 	}
 	
 	public byte[] read(int fileName) {
 		//DebugPrint.printString("read() in SS");
 		//DebugPrint.printString("allFilesNames.size() = " + allFilesNames.size());
-		//debug
-		//Enumeration<Integer> allFiles = allFilesNames.keys();
-		//while (allFiles.hasMoreElements())
-		//	DebugPrint.printString("found file " + allFiles.nextElement());
-
-		
-		
 		if (!allFilesNames.containsKey(fileName))
 			throw new IOException("The file " + fileName + " doesn't exists.");
 		else if (!files.containsKey(fileName))
@@ -141,14 +136,129 @@ public class SecureStorage {
 		modifiedFiles.remove(fileName);
 	}
 	
-	private byte[] encrypt(byte[] plainText) {
-		return Crypto.encrypt(plainText);
-	}
 	
-	private byte[] decrypt(byte[] cipherText) {
-		return Crypto.decrypt(cipherText);
+	static byte[] encrypt(byte[] plainText) {
+		//DebugPrint.printString("on Crypto.encrypt()");
+		//Java is PITA 
+		short indexZero = (short)0;
+		
+		// Create Platform Binded cipher
+		SymmetricBlockCipherAlg SymmetricCipher = SymmetricBlockCipherAlg.create(SymmetricBlockCipherAlg.ALG_TYPE_PBIND_AES_256_CBC);
+		
+		//Create and Initial the Hash(plainText)
+		HashAlg hashAlg = HashAlg.create(HashAlg.HASH_TYPE_SHA256);
+		short hashSize = hashAlg.getHashLength();
+		byte[] hash =new byte[hashSize];
+		//init
+		hashAlg.processComplete(plainText,indexZero,(short)plainText.length,hash,indexZero);
+				
+		//DebugPrint.printString("hash data:");
+		//DebugPrint.printBuffer(hash);
+		
+		//constart a byte array to hold the data to encrypt (Hash size | Hash | data size | data | Padded if neccesry to match bloack size)
+		int dataSize = TypeConverter.SHORT_BYTE_SIZE + hashSize + TypeConverter.INT_BYTE_SIZE + plainText.length;
+		// Align the data size to block buffer size
+		short blockSize = SymmetricCipher.getBlockSize();
+		if (dataSize % blockSize != 0)
+			dataSize = dataSize + blockSize - (dataSize % blockSize);
+		// An array to hold the data to encrypt
+		byte[] data = new byte[dataSize];
+		//init the array
+		int currentIndexInsideTheDataArray = indexZero;
+		// First bytes are the hashSize value
+		TypeConverter.shortToBytes(hashSize, data, currentIndexInsideTheDataArray);
+		currentIndexInsideTheDataArray += TypeConverter.SHORT_BYTE_SIZE;
+		// Second bytes are the hash
+		ArrayUtils.copyByteArray(hash, 0, data, currentIndexInsideTheDataArray, hashSize);
+		currentIndexInsideTheDataArray += hashSize;
+		// next bytes are the plainText length value
+		TypeConverter.intToBytes(plainText.length, data, currentIndexInsideTheDataArray);
+		currentIndexInsideTheDataArray += TypeConverter.INT_BYTE_SIZE;
+		// Then copy the plainText buffer to encrypt
+		ArrayUtils.copyByteArray(plainText, 0, data, currentIndexInsideTheDataArray, plainText.length);
+
+		// An array for the encrypted data, 
+		// Data size stays the same after encryption because we are using a symmetric key
+		byte[] encryptedData = new byte[dataSize];
+		
+		//Create and Initial the Initialization Vector for AES 256 CBC mode.
+		short ivSize = SymmetricCipher.getBlockSize();
+		byte[] iv = new byte[ivSize];
+		Random.getRandomBytes(iv, indexZero, ivSize);
+		//init
+		SymmetricCipher.setIV(iv, indexZero, ivSize);
+		
+		// Encrypt the data
+		SymmetricCipher.encryptComplete(data, indexZero, (short) dataSize, encryptedData, indexZero);
+		
+		// returnArray is IV | encryptedData
+		byte[] returnArray = new byte[ivSize+dataSize];
+		ArrayUtils.copyByteArray(iv, indexZero, returnArray, indexZero, ivSize);
+		ArrayUtils.copyByteArray(encryptedData, indexZero, returnArray, ivSize, dataSize);
+		
+		//DebugPrint.printString("returnArray data:");
+		//DebugPrint.printBuffer(returnArray);
+		
+		// Return the encrypted data to the host application
+		return returnArray;
 	}
-	
+
+	static byte[] decrypt(byte[] cipherText) {
+		//Java is PITA 
+		short indexZero = (short)0;
+		
+		// Create Platform Binded cipher
+		SymmetricBlockCipherAlg SymmetricCipher = SymmetricBlockCipherAlg.create(SymmetricBlockCipherAlg.ALG_TYPE_PBIND_AES_256_CBC);
+
+		//Create and Initial the Initialization Vector for AES 256 CBC mode using the chipher text firsts bytes.
+		short ivSize = SymmetricCipher.getBlockSize();
+		byte[] iv = new byte[ivSize];
+		ArrayUtils.copyByteArray(cipherText, indexZero, iv, indexZero, ivSize);
+		//init
+		SymmetricCipher.setIV(iv, indexZero, ivSize);
+
+		// An array for the decrypted data, 
+		// Data size stays is cipherText length minus the iv bytes in the start of the chperText
+		// decrypt data is: Hash size | Hash | data size | data | paded
+		byte[] decryptedData = new byte[cipherText.length - ivSize];		
+		
+		// Decrypt the data
+		SymmetricCipher.decryptComplete(cipherText, ivSize, (short) (cipherText.length - ivSize), decryptedData, indexZero);
+		
+		int currentIndexInsideTheDecryptedDataArray = indexZero;
+		
+		//old hash:
+		short oldHashSize = TypeConverter.bytesToShort(decryptedData, currentIndexInsideTheDecryptedDataArray);
+		currentIndexInsideTheDecryptedDataArray += TypeConverter.SHORT_BYTE_SIZE;
+		byte[] oldHash = new byte[oldHashSize];
+		ArrayUtils.copyByteArray(decryptedData, currentIndexInsideTheDecryptedDataArray, oldHash, indexZero, oldHashSize);
+		currentIndexInsideTheDecryptedDataArray += oldHashSize;
+		
+		//old data:
+		int decryptDataSize = TypeConverter.bytesToInt(decryptedData, currentIndexInsideTheDecryptedDataArray);
+		currentIndexInsideTheDecryptedDataArray += TypeConverter.INT_BYTE_SIZE;
+		byte[] plainText = new byte[decryptDataSize];
+		ArrayUtils.copyByteArray(decryptedData, currentIndexInsideTheDecryptedDataArray, plainText, indexZero, decryptDataSize);
+		
+		HashAlg hashAlg = HashAlg.create(HashAlg.HASH_TYPE_SHA256);
+		short hashSize = hashAlg.getHashLength();
+		byte[] hash =new byte[hashSize];
+		//init
+		hashAlg.processComplete(plainText,indexZero,(short)plainText.length,hash,indexZero);
+		
+		if(hashSize != oldHashSize || !ArrayUtils.compareByteArray(oldHash, indexZero, hash, indexZero, hashSize)) {
+			//DebugPrint.printString("Hash not match");
+			throw new CryptoException("Hash not match");
+		}
+		
+		//DebugPrint.printString("hash data:");
+		//DebugPrint.printBuffer(hash);
+		
+		//DebugPrint.printString("data:");
+		//DebugPrint.printBuffer(plainText);
+		return plainText;
+	}
+
 
 
 }
